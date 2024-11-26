fba1e8b65ceb429d77559dd6e4582e3e58d61c90
9k3pthhurh8f9z <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2024-11-27 03:19:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+import { Todo } from "./types";
+import { initTodos } from "./initTodos";
+import WelcomeMessage from "./WelcomeMessage";
+import TodoList from "./TodoList";
+import { v4 as uuid } from "uuid"; // ◀◀ 追加
+import dayjs from "dayjs"; // ◀◀ 追加
+import { twMerge } from "tailwind-merge"; // ◀◀ 追加
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // ◀◀ 追加
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"; // ◀◀ 追加
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>(initTodos);
+  const [newTodoName, setNewTodoName] = useState(""); // ◀◀ 追加
+  const [newTodoPriority, setNewTodoPriority] = useState(3); // ◀◀ 追加
+  const [newTodoDeadline, setNewTodoDeadline] = useState<Date | null>(null); // ◀◀ 追加
+  const [newTodoNameError, setNewTodoNameError] = useState("");
+
+  const uncompletedCount = todos.filter((todo: Todo) => !todo.isDone).length;
+  console.log(JSON.stringify(todos, null, 2));
+
+  // ▼▼ 追加
+  const isValidTodoName = (name: string): string => {
+    if (name.length < 2 || name.length > 32) {
+      return "2文字以上、32文字以内で入力してください";
+    } else {
+      return "";
+    }
+  };
+
+  // ▼▼ 追加
+  const updateNewTodoName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoNameError(isValidTodoName(e.target.value)); // ◀◀ 追加
+    setNewTodoName(e.target.value);
+  };
+
+  // ▼▼ 追加
+  const updateNewTodoPriority = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoPriority(Number(e.target.value)); // 数値型に変換
+  };
+  const updateDeadline = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const dt = e.target.value; // UIで日時が未設定のときは空文字列 "" が dt に格納される
+    console.log(`UI操作で日時が "${dt}" (${typeof dt}型) に変更されました。`);
+    setNewTodoDeadline(dt === "" ? null : new Date(dt));
+  };
+
+  const addNewTodo = () => {
+    // ▼▼ 編集
+    const err = isValidTodoName(newTodoName);
+    if (err !== "") {
+      setNewTodoNameError(err);
+      return;
+    }
+    const newTodo: Todo = {
+      id: uuid(),
+      name: newTodoName, // ◀◀ 編集
+      isDone: false,
+      priority: newTodoPriority, // ◀◀ 追加
+      deadline: newTodoDeadline, // ◀◀ 追加
+    };
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+    setNewTodoName(""); // ◀◀ 追加
+    setNewTodoPriority(3); // ◀◀ 追加
+    setNewTodoDeadline(null); // ◀◀ 追加
+  };
+  const currentDate = dayjs().format("YYYY年MM月DD日 H時m分");
+
+  return (
+    <div className="bg-gray-500">
+      <div className="mx-4 max-w-2xl md:mx-auto">
+        <h1 className="mb-4 text-7xl font-bold">やること管理</h1>
+        <div className="mb-4 font-bold">今は{currentDate}です</div>
+        <div className="mb-4">
+          <WelcomeMessage
+            name="寝屋川タヌキ"
+            uncompletedCount={uncompletedCount}
+          />
+        </div>
+
+        {/* タスク追加関連のUI実装 ここから... */}
+        <div className="mt-5 space-y-2 rounded-md border bg-white p-3">
+          <h2 className="text-lg font-bold">新しいタスクの追加</h2>
+          {/* 編集: ここから... */}
+          <div>
+            <div className="flex items-center space-x-2">
+              <label className="font-bold" htmlFor="newTodoName">
+                名前
+              </label>
+              <input
+                id="newTodoName"
+                type="text"
+                value={newTodoName}
+                onChange={updateNewTodoName}
+                className={twMerge(
+                  "grow rounded-md border p-2",
+                  newTodoNameError && "border-red-500 outline-red-500"
+                )}
+                placeholder="2文字以上32文字以内"
+              />
+            </div>
+            {newTodoNameError && (
+              <div className="ml-10 flex items-center space-x-1 text-sm font-bold text-red-500 ">
+                <FontAwesomeIcon
+                  icon={faTriangleExclamation}
+                  className="mr-0.5"
+                />
+                <div>{newTodoNameError}</div>
+              </div>
+            )}
+          </div>
+          {/* ...ここまで */}
+
+          {/* ラジオボタンの実装 ここから... */}
+          <div className="flex gap-5">
+            <div className="font-bold">優先度</div>
+            {[1, 2, 3].map((value) => (
+              <label key={value} className="flex items-center space-x-1">
+                <input
+                  id={`priority-${value}`}
+                  name="priorityGroup"
+                  type="radio"
+                  value={value}
+                  checked={newTodoPriority === value}
+                  onChange={updateNewTodoPriority}
+                />
+                <span>{value}</span>
+              </label>
+            ))}
+          </div>
+          {/* ...ここまで */}
+          {/* DateTimeUIの実装 ここから... */}
+          <div className="flex items-center gap-x-2">
+            <label htmlFor="deadline" className="font-bold">
+              期限
+            </label>
+            <input
+              type="datetime-local"
+              id="deadline"
+              value={
+                newTodoDeadline
+                  ? dayjs(newTodoDeadline).format("YYYY-MM-DDTHH:mm:ss")
+                  : ""
+              }
+              onChange={updateDeadline}
+              className="rounded-md border border-gray-400 px-2 py-0.5"
+            />
+          </div>
+          {/* ...ここまで */}
+
+          <button
+            type="button"
+            onClick={addNewTodo} // ボタンを押下したときの処理
+            className="rounded-md bg-indigo-500 px-3 py-1 font-bold text-white hover:bg-indigo-600"
+          >
+            追加
+          </button>
+        </div>
+        <TodoList todos={todos} />
+        {/* ...ここまで */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/TodoList.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import dayjs from "dayjs";
+import { FaStar } from "react-icons/fa";
+
+const TodoList = (props: Props) => {
+  const todos = [...props.todos].sort((a, b) => a.priority - b.priority);
+
+  if (todos.length === 0) {
+    return (
+      <div className="text-red-500">
+        現在、登録されているタスクはありません。
+      </div>
+    );
+  }
+
+  const calculateRemainingDays = (deadline: Date) => {
+    const now = dayjs();
+    const end = dayjs(deadline);
+    return end.diff(now, "day");
+  };
+
+  const getPriorityLabel = (priority: number) => {
+    switch (priority) {
+      case 5:
+        return "!!超重要!!";
+      case 4:
+        return "重要!";
+      case 3:
+        return "";
+      case 2:
+        return "あまり気にせず";
+      case 1:
+        return "一旦忘れていい";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div className="space-y-5">
+      {todos.map((todo) => {
+        const remainingDays = todo.deadline
+          ? calculateRemainingDays(todo.deadline)
+          : null;
+        const isOverdue = remainingDays !== null && remainingDays <= 0;
+
+        return (
+          <div
+            className={`relative rounded-2xl border-8 bg-gradient-to-r from-red-100 to-blue-100 p-4 shadow-xl ${isOverdue ? "border-red-500" : "border-green-500"} ${todo.isDone ? "opacity-50" : ""}`}
+            key={todo.id}
+          >
+            {todo.isDone && (
+              <img
+                src="/circle-3.svg"
+                alt="Completed"
+                className="absolute inset-0 size-full object-cover opacity-20"
+              />
+            )}
+            <div
+              className={`text-5xl font-bold ${isOverdue ? "text-red-500" : ""}`}
+            >
+              {isOverdue ? `***${todo.name}***` : todo.name}
+            </div>
+            <div className="flex items-center text-2xl">
+              優先度{" "}
+              {Array(todo.priority)
+                .fill(0)
+                .map((_, i) => (
+                  <FaStar key={i} className="text-yellow-500" />
+                ))}
+              <span className="ml-2">{getPriorityLabel(todo.priority)}</span>
+            </div>
+            <div className="text-2xl">
+              {" "}
+              {todo.deadline
+                ? dayjs(todo.deadline).format("YYYY年MM月DD日 H時m分")
+                : "いつまでも"}
+              まで
+            </div>
+            <div className="text-2xl">
+              あと{todo.deadline ? `${remainingDays}日` : "何日とかないよー"}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
